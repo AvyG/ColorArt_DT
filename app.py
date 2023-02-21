625d621182942669a9bf9f57d47f6f0cd2313a04
Serene <luckyseorin@gmail.com>
2023-02-21 22:11:28 +0100

    test

--- app.py
+++ app.py
@@ -56,16 +56,16 @@ with tab_aboutus:
     col1, col2, col3, col4 = st.columns(4, gap="medium")
     with col1:
         st.image('memoji_seorin.png',  use_column_width='auto')
-        st.markdown ("Seorin Kim <\n> seorin.kim@student.kuleuven.be")
+        st.markdown ("Seorin Kim \n> seorin.kim@student.kuleuven.be")
     with col2:
         st.image('logo.png',  use_column_width=True)
-        st.markdown ("Seorin Kim <\n> seorin.kim@student.kuleuven.be")
+        st.markdown ("Seorin Kim \n> seorin.kim@student.kuleuven.be")
     with col3:
         st.image('logo.png',  use_column_width=True)
-        st.markdown ("Seorin Kim <\n> seorin.kim@student.kuleuven.be")
+        st.markdown ("Seorin Kim \n> seorin.kim@student.kuleuven.be")
     with col4:
         st.image('logo.png',  use_column_width=True)
-        st.markdown ("Seorin Kim <\n> seorin.kim@student.kuleuven.be")
+        st.markdown ("Seorin Kim \n> seorin.kim@student.kuleuven.be")
                 
     
     
