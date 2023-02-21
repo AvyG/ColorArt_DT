97ab38d7f9462968e980e7ee75cb62006d170ecd
Avy <afgarcia1@uc.cl>
2023-02-21 23:53:03 +0100

    First structure rep
    Add directories, route paths, reshape images and add script for tabs

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-import pandas as pd
 import streamlit as st
-import time
-from data import *
+from utils.functions import *
 
 # Set page title
 st.set_page_config( page_title='Art-IA color study', 
@@ -18,10 +16,8 @@ with st.sidebar:
 
 if add_selectbox == "Home":
     # Home Page
-    st.title("Color Relatioships in Art and AI-generated Art")
-    st.write("The aim of this project is to explore the relationships "+
-            "between the most prominent colors in man-made and "+
-            "AI-generated works with an specific style or artist"
+    st.title("Color Relationships in Art and AI-Generated Art")
+    st.write("The aim of this project is to explore the relationships between the most prominent colors in man-made and AI-generated works with an specific style or artist"
             )
     
     # n pairs of images from artwork_both_filtered.csv
@@ -31,10 +27,17 @@ if add_selectbox == "Home":
     # Display the images side-by-side
     with st.container():
         col1, col2 = st.columns(2)
-        col1.image(call_artwork(image_list[0][0]), use_column_width=True)
+        image1 = call_artwork(image_list[0][0]).resize((500,500))
+        col1.image(image1, use_column_width=True)
         col2.image(call_artwork(image_list[0][1]), use_column_width=True)
 
 
 elif add_selectbox == "Exploratory analysis":
+    st.title("Color Relationships in Art and AI-Generated Art")
     st.write("changepage"
-         )
+            )
+
+elif add_selectbox == "Color Analysis":
+    st.title("Color Relationships in Art and AI-Generated Art")
+    st.write("In this section, we present the color extraction of multiple artworks"
+            )
---
+++ utils/functions.py
@@ -0,0 +1,29 @@
+import csv
+import io
+import pathlib as p
+import random
+import requests
+from PIL import Image
+
+# Find current directory
+app_path = p.Path(__file__).parents[1]
+
+# Useful path and files
+data_path = p.PurePath.joinpath(app_path, 'data')
+p_filtered_artwork = p.PurePath.joinpath(data_path, 'artwork_both_filtered.csv')
+
+# This function creates returns one list of n elements that half
+# are the artworks url and the other half are AI works to display
+def load_image_list(n):
+    table = []
+    with io.open(p_filtered_artwork, 'r', encoding="Windows-1254", errors='replace') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            table.append((row[5],row[13]))
+    random_elements = random.sample(table, n)
+    return random_elements
+
+def call_artwork(url):
+    response = requests.get(url)
+    image = Image.open(io.BytesIO(response.content))
+    return image
