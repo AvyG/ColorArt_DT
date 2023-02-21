7c0fdefbc0bc0407db6b2b5ef9018d9cfd83b98a
Serene <luckyseorin@gmail.com>
2023-02-21 22:02:34 +0100

    file name (test)

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -55,16 +55,16 @@ with tab_aboutus:
     
     col1, col2, col3, col4 = st.columns(4, gap="medium")
     with col1:
-        st.image('memoji_seorin.png',  use_column_width=True)
+        st.image('logo.png',  use_column_width=True)
         st.markdown ("Seorin Kim <\n> seorin.kim@student.kuleuven.be")
     with col2:
-        st.image('memoji_seorin.png',  use_column_width=True)
+        st.image('logo.png',  use_column_width=True)
         st.markdown ("Seorin Kim <\n> seorin.kim@student.kuleuven.be")
     with col3:
-        st.image('memoji_seorin.png',  use_column_width=True)
+        st.image('logo.png',  use_column_width=True)
         st.markdown ("Seorin Kim <\n> seorin.kim@student.kuleuven.be")
     with col4:
-        st.image('memoji_seorin.png',  use_column_width=True)
+        st.image('logo.png',  use_column_width=True)
         st.markdown ("Seorin Kim <\n> seorin.kim@student.kuleuven.be")
                 
     
