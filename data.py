57514727c7a2b41ec6f81ead82e5bf2425dc4ff7
Avy <afgarcia1@uc.cl>
2023-02-21 12:13:31 +0100

    Adding pathlib to data.py

--- data.py
+++ data.py
@@ -1,15 +1,21 @@
 import csv
 import io
+import pathlib as p
 import random
 import requests
 from PIL import Image
 
+# Find current directory
+file_path = p.Path(__file__).parent
+
+# Useful files
+p_filtered_artwork = p.PurePath.joinpath(file_path, 'artwork_both_filtered.csv')
+
 # This function creates returns one list of n elements that half
 # are the artworks url and the other half are AI works to display
-
 def load_image_list(n):
     table = []
-    with io.open('artwork_both_filtered.csv', 'r', encoding="Windows-1254", errors='replace') as csvfile:
+    with io.open(p_filtered_artwork, 'r', encoding="Windows-1254", errors='replace') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             table.append((row[5],row[13]))
