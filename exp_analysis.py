da9f330ad1678bfab5b8f2ab329b251cbe80864d
Avy <afgarcia1@uc.cl>
2023-02-23 08:36:12 +0100

    Continuation of exploration data
    portrait plus fixing bugs

--- exp_analysis.py
+++ exp_analysis.py
@@ -1,3 +1,4 @@
+import math
 import pandas as pd
 import pathlib as p
 import streamlit as st
@@ -27,53 +28,67 @@ def create_exppage():
                  'source_artwork','topic_AI','artist_AI',
                  'url_wiki','wiki_artist','image_url','url_AI','url_picture']
 
-    #display_data = display_data.reindex(columns=new_order)
+    display_data = display_data.reindex(columns=new_order)
 
     st.header("Input table")
     st.markdown("This is the data we use to based or research")
-    st.dataframe(display_data)
+    with st.expander("**Expand/Contract Here**", expanded=True):
+        st.dataframe(display_data)
+
+
 
     st.header("Query by Artist")
     
     artwork_counts = data['artist_name'].value_counts().to_frame().reset_index()
     artwork_counts.columns = ['Artist Name', 'Artwork Count']
+    
+    with st.expander("**Expand/Contract Here** (⚠️ It takes some time to run)", expanded=True):
+        list_artist = data['artist_name'].unique().tolist()
+        list_artist.sort()
 
-    list_artist = ["Vincent Van Gogh", "Sandro Botticelli", "Frida Kahlo" ] 
+        with st.container():
+            artist = st.selectbox("Select Artist",
+                                list_artist, index=161)
 
-    with st.container():
-        artist = st.selectbox("Select Artist",
-                             list_artist)
+            artist_data = data.loc[data['artist_name'] == artist]
 
-        artist_data = data.loc[data['artist_name'] == artist]
 
-        movement_lists = artist_data['artist_movements'].tolist()
-        store_clean = []
-        for mov_string in movement_lists:
-            clean_str = mov_string.replace('[','').replace(']','').replace('\'','')
-            store_clean.extend(clean_str.split(','))
-        movement_str = ', '.join(list(set(store_clean)))
-
-        info_dict = {
-                'Artworks': [artwork_counts.loc[artwork_counts['Artist Name'] == artist].values[0][1]],
-                'Movements': [movement_str]
-            }
-        summary = artist_data['summary_artist'].values[0]
-
-        col1, col2 = st.columns(2)
-        with col1:
-            st.subheader('Information available')
-            st.table(pd.DataFrame.from_dict(info_dict, orient='index', columns=['']))
-            st.text_area("**Summary of the Artist**", summary, height=300)
-
-        with col2:
-            st.subheader('Portrait')
-            subcol1, subcol2, subcol3 = st.columns([1,5,1])
-            if type(artist_data['picture'].values[0]) == int or float:
-                image = call_image('noportrait.png').resize((500,500))
-                subcol2.image(image, width=250)
+            movement_lists = artist_data['artist_movements'].tolist()[0]        
+            if isinstance(movement_lists, str):
+                store_clean = []
+                clean_str = movement_lists.replace('[','').replace(']','').replace('\'','')
+                store_clean.extend(clean_str.split(','))
+                movement_str = ', '.join(list(set(store_clean)))
             else:
-                st.write('future portrait')
-            st.subheader('Gallery')
-            subcol4, subcol5, subcol6 = st.columns(3)
+                movement_str = 'No Information'
+
+            info_dict = {
+                    'Artworks': [artwork_counts.loc[artwork_counts['Artist Name'] == artist].values[0][1]],
+                    'Movements': [movement_str]
+                }
+
+            summary = artist_data['summary_artist'].values[0]
+            if isinstance(summary, float) and math.isnan(summary):
+                summary = 'No Information'
+
+            col1, col2 = st.columns(2)
+            with col1:
+                st.subheader('Information available')
+                st.table(pd.DataFrame.from_dict(info_dict, orient='index', columns=['']))
+                st.text_area("**Summary of the Artist**", summary, height=300)
+
+            with col2:
+                st.subheader('Portrait')
+                subcol1, subcol2, subcol3 = st.columns([1,5,1])
+                pic_value = artist_data['picture'].values[0]
+                if not math.isnan(pic_value):
+                    url_prt = data.loc[data['artist_name'] == artist]['url_picture'].values[0]
+                    image = get_artwork(url_prt).resize((500,500))
+                    subcol2.image(image, width=250)
+                else:
+                    image = call_image('noportrait.png').resize((500,500))
+                    subcol2.image(image, width=250)
+                st.subheader('Gallery')
+                subcol4, subcol5, subcol6 = st.columns(3)
 
 
